27d46dd6c1a0f1d53dc15c3be649cfc8534aaf05
laibaidrees10 <laibaidrees44@gmail.com>
2025-05-15 21:45:51 +0500

    Update App.jsx

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,13 +1,1 @@
-// app/javascript/components/App.jsx
-import React from "react";
-import Dashboard from "./Dashboard";
 
-function App() {
-  return (
-    <div className="app">
-      <Dashboard />
-    </div>
-  );
-}
-
-export default App;
